6e05fb187a2e73c5212e06663acaf19022aec354
Milán Kormány <kormany.1993@gmail.com>
2020-05-22 09:44:47 +0200

    impute close statment was missing

--- workspace/fighting-fantasy/src/run.java
+++ workspace/fighting-fantasy/src/run.java
@@ -16,6 +16,7 @@ public class run {
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
+        input.close();
 		System.out.println("\"Üdvözöllek!\" - Mondta a törpe a bajszát megpenderitve. \"Sokan megfordultak már erre felé, de tudod mindnyájan elbuktak. Talán te leszel a serencsés aki legyõzi a szörnyet!\"");
 		System.out.println("\"Indulj északnak, arra leled a szerencséd!\" A kalandor el is indult. Késõbb rátalált a szörny barlangjára. Meg is támadta a fene nagy bestiát aki vissza támadta ellenfelét!");
 		System.out.println("Harcra fel!");
